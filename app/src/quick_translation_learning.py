881656d0e25acbb91f0f2761bcd44db4989d36eb
hikaru <hika1785040525@gmail.com>
2026-02-21 21:18:23 +0900

    Add first prototype
    Signed-off-by: hikaru <hika1785040525@gmail.com>

---
+++ app/src/quick_translation_learning.py
@@ -0,0 +1,104 @@
+import csv
+import random
+import time
+import argparse
+import os
+APP_ABS_PATH = "/Users/sakuraihikari/quick_translation_learning/app"
+INPUT_DIR_PATH = os.path.join(APP_ABS_PATH, "input")
+DEFAULT_DATA_CSV_PATH =  os.path.join(INPUT_DIR_PATH, "quick_translation_data.csv")
+DEFAULT_TIME_LIMIT = 5
+MAX_QUESTIONS  = 50
+
+def load_csv_file(file_path: str):
+    with open(file_path, newline="", encoding="utf-8") as f:
+        data = csv.reader(f)
+        return list(data)
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def run_quick_translation_learning(data: list, time_limit: float=DEFAULT_TIME_LIMIT, max_questions: int=MAX_QUESTIONS):
+    # shuffle data for random order
+    random.shuffle(data)
+
+    # run the quiz and collect slow questions
+    slow_questions = []
+    for i, (jp, en) in enumerate(data):
+        num_q=i+1
+        print("\n==============================")
+        print(f"Question-{num_q}")
+        print("日本語：", jp)
+
+        start = time.perf_counter()
+        input("▶ 英語を言ったら Enter: ")
+        elapsed = time.perf_counter() - start
+
+        print("English：", en)
+        print(f"⏱ {elapsed:.2f} 秒")
+
+        if elapsed > time_limit:
+            print("⚠️ 時間オーバー！")
+            slow_questions.append((jp, en))
+
+        input("▶ Next! Press Enter: ")
+        clear_screen()
+        if num_q >= max_questions:
+            break
+
+    print(f"\n⏱ Slow questions: {len(slow_questions)}")
+    return slow_questions
+
+def quick_translation_learning(file_path: str, time_limit: float = 1.5):
+    rows = load_csv_file(file_path)
+
+    if len(rows) < 2:
+        print("Please put correct data")
+        return
+
+    _, question_list = rows[0], rows[1:]
+
+    input("▶ Are you ready? Press Enter to start.")
+    loop_count = 0
+    while question_list:
+        if loop_count > 1:
+            print("\n🔁 Let's try the slow questions again!")
+        question_list = run_quick_translation_learning(question_list, time_limit)
+        loop_count += 1
+
+    print("\n✅ 全ての問題をクリア！お疲れ様でした！")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Instant English - Quick Translation Learning"
+    )
+    parser.add_argument(
+        "--input_file_path",
+        type=str,
+        default=DEFAULT_DATA_CSV_PATH,
+        help="Path of CSV file containing Japanese and English sentences"
+    )
+    parser.add_argument(
+        "--time_limit",
+        type=float,
+        default=DEFAULT_TIME_LIMIT,
+        help="Time limit in seconds"
+    )
+    parser.add_argument(
+        "--max_questions",
+        type=int,
+        default=MAX_QUESTIONS,
+        help="Maximum number of questions per round"
+    )
+
+    args = parser.parse_args()
+
+    quick_translation_learning(
+        file_path=args.input_file_path,
+        time_limit=args.time_limit
+        max_questions=args.max_questions
+    )
+
+
+if __name__ == "__main__":
+    main()
