bcd991aae5aebe158c18b6cabfb0f2a52691b455
hikaru <hika1785040525@gmail.com>
2026-03-01 11:59:00 +0900

    Add new phrase

--- app/src/default_definitions.py
+++ app/src/default_definitions.py
@@ -1,8 +1,6 @@
 import os
-
-
-
-APP_ABS_PATH = "/Users/sakuraihikari/quick_translation_learning/app"
+GIT_ABS_PATH = os.environ.get("GIT_ABS_PATH", "/Users/sakuraihikari")
+APP_ABS_PATH = os.path.join(GIT_ABS_PATH, "quick_translation_learning", "app")
 INPUT_DIR_PATH = os.path.join(APP_ABS_PATH, "input")
 DEFAULT_DATA_CSV_PATH =  os.path.join(INPUT_DIR_PATH, "quick_translation_data.csv")
 DEFAULT_TIME_LIMIT = 5
